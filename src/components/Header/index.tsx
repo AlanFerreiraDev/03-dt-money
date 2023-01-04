a555364093f6f7eb09df0a9796f2d62fac1b0d24
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-01-04 09:21:35 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
+
+export const Header = () => {
+  return (
+    <header>
+      <HeaderContainer>
+        <HeaderContent>
+          <img src={logoImg} alt="dt money" />
+
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+            <NewTransactionModal />
+          </Dialog.Root>
+        </HeaderContent>
+      </HeaderContainer>
+    </header>
+  )
+}
